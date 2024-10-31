14c4286bae80ae52e389adfe2521620852be2564
alantan493 <alan.tan30@hotmail.com>
2024-10-31 21:57:55 +0800

    Add files via upload

--- facetherealityUI/script.js
+++ facetherealityUI/script.js
@@ -1,7 +1,7 @@
 // Function to redirect to a specific URL when the button is clicked
 function redirectToDownload() {
     // Change this URL to the page or site you want to redirect to
-    window.location.href = 'https://entuedu-my.sharepoint.com/personal/geor0014_e_ntu_edu_sg/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fgeor0014%5Fe%5Fntu%5Fedu%5Fsg%2FDocuments%2FDIP%20E028%2FARware%20Game%2FDrugARwarenessGameForIntegration%2Eapk&parent=%2Fpersonal%2Fgeor0014%5Fe%5Fntu%5Fedu%5Fsg%2FDocuments%2FDIP%20E028%2FARware%20Game&ga=1';
+    window.location.href = 'https://entuedu-my.sharepoint.com/:u:/g/personal/tanj0304_e_ntu_edu_sg/EcxZTKLmkFZBqYBdQi5Pch4BRl4GJQqL4GbUSL4-rorWtA?e=eElu3q';
 }
 
 // Adding event listener to the button
